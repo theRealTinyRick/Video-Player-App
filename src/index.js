e21a89fac0eda4fe6e0d15c9b2d17da9a5d213d1
Aaron Hines <31013885+theRealTinyRick@users.noreply.github.com>
2018-03-10 20:33:43 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js'
+const API_KEY = 'AIzaSyBK7ctQMocL-Nq7-Vn9b7Qa67XAWyHvpKM';
+
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            videos:[],
+            selectedVideo: null
+        };
+        this.videoSearch('bjj highlights');
+    }
+    
+    videoSearch(term){
+        YTSearch({key: API_KEY, term:term}, (videos) => {
+            this.setState({
+                videos:videos,
+                selectedVideo:videos[0]
+            });
+        });        
+    }
+    
+    render(){
+        const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300)
+        
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video = {this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect = {selectedVideo=>this.setState({selectedVideo})}
+                    videos = {this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
